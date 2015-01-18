0d9aaefc72e7abef1b69789a8677e5eaba40a2ac
Roudoudou <clmtlucas@gmail.com>
2015-01-18 19:33:54 +0100

    Chapter 11 The Root of all Evil

--- src/test/java/Money/MoneyTest.java
+++ src/test/java/Money/MoneyTest.java
@@ -13,18 +13,12 @@ public class MoneyTest {
         Money five = Money.dollar(5);
         assertEquals(Money.dollar(10), five.times(2));
         assertEquals(Money.dollar(15), five.times(3));
-
-        Money fivefranc = Money.franc(5);
-        assertEquals(Money.franc(10), fivefranc.times(2));
-        assertEquals(Money.franc(15), fivefranc.times(3));
     }
 
     @Test
     public void testEquality(){
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-        assertTrue(Money.franc(5).equals(Money.franc(5)));
-        assertFalse(Money.franc(5).equals(Money.franc(6)));
 
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
@@ -33,10 +27,5 @@ public class MoneyTest {
     public void testCurrency() {
         assertEquals("USD", Money.dollar(1).currency());
         assertEquals("CHF", Money.franc(1).currency());
-    }
-
-    @Test
-    public void testDifferentClassEquality(){
-        assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
     }
 }
